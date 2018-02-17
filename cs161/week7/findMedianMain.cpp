8c757aaa6797e29a627678b1c22a81aa94c020fe
Tony Huynh <tonyduy092@gmail.com>
2018-02-16 22:44:02 -0800

    Adding Assignment 7a, findMedian.cpp

---
+++ cs161/week7/findMedianMain.cpp
@@ -0,0 +1,23 @@
+/*********************************************************************
+** Author: Tony Huynh
+** Date: 2/21/2018
+** Description: findMedian.cpp Unit Test
+*********************************************************************/
+#include <gtest/gtest.h>
+
+// Function prototype
+double findMedian(int array[], int size);
+
+const int oddArray[] = { 9, -13, 9, 11, 9, 13, -11, 9, 0, 8, 11 };
+const int evenArray[] = { 5, 11, 12, 4, 8, 21 };
+const int ODD_ARRAY_SIZE = sizeof(oddArray) / sizeof(oddArray[0]);
+const int EVEN_ARRAY_SIZE = sizeof(evenArray) / sizeof(evenArray[0]);
+
+TEST(findMedianTest, oddArrayTrue) {
+
+}
+
+int main() {
+
+	return 0;
+}
