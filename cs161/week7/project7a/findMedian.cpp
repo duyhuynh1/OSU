b013291c6098dada56d23f74177783767e57d920
Tony Huynh <tonyduy092@gmail.com>
2018-02-20 22:02:19 -0800

    new commit

--- cs161/week7/project7a/findMedian.cpp
+++ cs161/week7/project7a/findMedian.cpp
@@ -14,11 +14,11 @@ double findMedian(int intArray[], int size) {
 	std::sort(intArray, intArray + size);	
 	if (size % 2 == 0) {
 		// Find the median of an even number array			
-		double central1 = intArray[size/2 - 1];
-		double central2 = intArray[size/2];
+		double central1 = intArray[size / 2 - 1];
+		double central2 = intArray[size / 2];
 		return median = (central1 + central2) / 2;
 	} else {
 		// Find the median of an odd number array								
-		return median = intArray[size/2];
+		return median = intArray[size / 2];
 	}
 }
--- cs161/week7/project7b/stdDev.cpp
+++ cs161/week7/project7b/stdDev.cpp
@@ -13,9 +13,9 @@ double stdDev(Person [], int);
 double stdDev(Person people[], int size) {
 	double result;
 	double totalAge = 0.0,
-		   mean = 0.0,
-		   summation = 0.0,
-		   variance = 0.0;
+               mean = 0.0,
+               summation = 0.0,
+               variance = 0.0;
 	// Calculate the mean age of the group of people
 	for (int i = 0; i < size; i++) {
 		totalAge += people[i].getAge();
