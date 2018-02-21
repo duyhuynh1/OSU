1dfda21d7095f47d4b171a136e5fa99540856464
Tony Huynh <tonyduy092@gmail.com>
2018-02-20 21:44:36 -0800

    Changed multiple variable initialization

--- cs161/week7/project7b/stdDev.cpp
+++ cs161/week7/project7b/stdDev.cpp
@@ -12,10 +12,10 @@ double stdDev(Person [], int);
 
 double stdDev(Person people[], int size) {
 	double result;
-	double totalAge = 0.0;
-	double mean = 0.0;
-	double summation = 0.0;
-	double variance = 0.0;
+	double totalAge = 0.0,
+		   mean = 0.0,
+		   summation = 0.0,
+		   variance = 0.0;
 	// Calculate the mean age of the group of people
 	for (int i = 0; i < size; i++) {
 		totalAge += people[i].getAge();
